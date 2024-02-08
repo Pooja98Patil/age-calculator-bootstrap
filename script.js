4671422b7d94f7398595e60c8cb88385220c5dd5
Pooja98Patil <ukpooja98@gmail.com>
2024-02-08 17:15:19 +0530

    Added Html code
    Added Html code

---
+++ script.js
@@ -0,0 +1,6 @@
+///////////////////////getting the values from html using query selector////////////////
+
+var dob  = document.querySelector("dob");
+var result = document.querySelector("calculate");
+var reset = document.querySelector("reset");
+
